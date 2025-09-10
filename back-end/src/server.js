9be7f9e65d3ca1b806b5631a01e3068fa64291df
moeawad11 <moeawad2004@hotmail.com>
2025-09-10 10:58:52 +0300

    Add MongoDB for data persistence

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,24 @@
 import express from 'express';
+import {MongoClient, ReturnDocument, ServerApiVersion} from "mongodb";
+import { version } from 'react';
+
+let db;
+
+async function connectToDB(){
+  const uri = 'mongodb://127.0.0.1/27017';
+
+  const client = new MongoClient(uri, {
+    ServerApi:{
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true
+    }
+  });
+
+  await client.connect();
+
+  db = client.db("full-stack-react-db");
+}
 
 const articleInfo = [
   {name: 'learn-react',upvotes:0, comments:[]},
@@ -10,26 +30,46 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 
-app.post("/api/articles/:name/upvote",(req,res)=>{
-  const article = articleInfo.find(a=>a.name===req.params.name);
-  article.upvotes++; 
-  res.send({article});
+
+app.get("/api/articles/:name", async (req,res)=>{
+  const {name} = req.params;
+
+  const article = await db.collection('articles').findOne({name});
+
+  res.send(article);
 });
 
-app.post("/api/articles/:name/comments",(req,res)=>{
+app.post("/api/articles/:name/upvote",async (req,res)=>{
+  const {name} = req.params;
+  const updatedArticle = await db.collection("articles").findOneAndUpdate({name}, {
+    $inc: {upvotes: 1}
+  },{
+    returnDocument: "after"
+  })
+  res.send(updatedArticle);
+});
+
+app.post("/api/articles/:name/comments",async (req,res)=>{
   const {name} = req.params;
   const {postedBy, text} = req.body;
+  const newComments = {postedBy,text};
 
-  const article = articleInfo.find(a=>a.name===req.params.name);
 
-  article.comments.push({
-    postedBy,
-    text,
+  const updatedArticle = await db.collection("articles").findOneAndUpdate({name},{
+    $push: {comments: newComments}
+  },{
+    returnDocument: "after"
   });
 
-  res.send({article});
+  res.send(updatedArticle);
 });
 
-app.listen(PORT, ()=>{
-  console.log(`Server listening on port ${PORT}...`);
-});
+async function start(){
+  await connectToDB();
+
+  app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}...`);
+  });
+}
+
+start();
